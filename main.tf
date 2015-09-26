fb449866c1cbd01ab8c0bebb04463a7058fa103d
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:11:43 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 /* Public subnet */
 resource "aws_subnet" "sbn_public" {
   vpc_id            = "${var.vpc_id}"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "vpc_id" {}
 variable "availability_zone" {}
 variable "sbn_cidr" {}
 variable "sbn_name" {}
+/* provider */
+variable "region"               {}
+variable "access_key"           {}
+variable "secret_key"           {}
